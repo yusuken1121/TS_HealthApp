2821695efd49cbfbf35e0e5615a5a7e5228d5f55
yusuken1121 <yusuken1121@gmail.com>
2024-05-12 12:11:05 -0700

    Separate the Classes

--- food-app.ts
+++ food-app.ts
@@ -3,9 +3,18 @@ class Score {
 }
 
 class Food {
-
+    constructor(public element: HTMLDivElement) {
+        element.addEventListener("click",() => {})
+    }
 }
 
 class Foods {
-    
+    elements = document.querySelectorAll<HTMLDivElement>('.food') 
+    constructor(){
+        this.elements.forEach(element => {
+
+        })
+    }
 }
+
+const foods = new Foods();
