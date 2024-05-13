03a1bde60a6690bea1911fd52425664ca8bf0ba5
yusuken1121 <yusuken1121@gmail.com>
2024-05-13 11:26:36 -0700

    Get scores and push into the array

--- food-app.js
+++ food-app.js
@@ -1,46 +1,42 @@
+"use strict";
 class Score {
-
 }
-
 class Food {
-    constructor(public element: HTMLDivElement) {
+    constructor(element) {
+        this.element = element;
         element.addEventListener("click", this.clickEventHandler.bind(this));
     }
     clickEventHandler() {
         this.element.classList.toggle('food--active');
     }
 }
-
 class Foods {
-    elements = document.querySelectorAll<HTMLDivElement>('.food');
-    private _activeElements: HTMLDivElement[] = []
-    private _activeElementsScore: number[] = []
-    get activeElements(){
+    get activeElements() {
         this._activeElements = [];
         this.elements.forEach(element => {
-            if(element.classList.contains('food--active')){
-                this._activeElements.push(element)
+            if (element.classList.contains('food--active')) {
+                this._activeElements.push(element);
             }
-        })
+        });
         return this._activeElements;
     }
-
-    get activeElementScore(){
+    get activeElementScore() {
         this._activeElementsScore = [];
         this.elements.forEach(element => {
             const foodScore = element.querySelector('.food__score');
-            if(foodScore){
+            if (foodScore) {
                 this._activeElementsScore.push(Number(foodScore.textContent));
             }
-        })
-        return this._activeElementsScore
+        });
+        return this._activeElementsScore;
     }
-
-    constructor(){
+    constructor() {
+        this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
+        this._activeElementsScore = [];
         this.elements.forEach(element => {
             new Food(element);
-        })
+        });
     }
 }
-
 const foods = new Foods();
